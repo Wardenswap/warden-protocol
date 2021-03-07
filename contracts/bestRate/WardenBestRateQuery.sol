a3d74a692424f4d1b6faec29f7a4a8cacf398860
Arc Warden <wardenswap@gmail.com>
2021-03-08 01:16:06 +0700

    refactoring getRate

--- contracts/bestRate/WardenBestRateQuery.sol
+++ contracts/bestRate/WardenBestRateQuery.sol
@@ -25,6 +25,28 @@ contract WardenBestRateQuery {
         warden = _warden;
     }
 
+    function _getRate(
+        IERC20  src,
+        IERC20  dest,
+        uint256 srcAmount,
+        uint256 route
+    )
+    private
+    view
+    returns (
+        uint256 amountOut
+    ) {
+        // fail-safe getting rate, equal to
+        // uint256 _amountOut = warden.getDestinationReturnAmount(routes[i], src, dest, srcAmount, partnerIndex);
+        bytes memory payload = abi.encodeWithSignature("getDestinationReturnAmount(uint256,address,address,uint256,uint256)", route, src, dest, srcAmount, partnerIndex);
+        (bool success, bytes memory data) = address(warden).staticcall(payload);
+        if (success) {
+            return abi.decode(data, (uint256));
+        } else {
+            return 0;
+        }
+    }
+
     function oneRoute(
         IERC20  src,
         IERC20  dest,
@@ -33,18 +55,16 @@ contract WardenBestRateQuery {
     )
     external
     view
-    returns (uint256 routeIndex, uint256 amountOut) {
+    returns (
+        uint256 routeIndex,
+        uint256 amountOut
+    ) {
         for (uint256 i = 0; i < routes.length; i++) {
-            // fail-safe getting rate
-            // uint256 _amountOut = warden.getDestinationReturnAmount(routes[i], src, dest, srcAmount, partnerIndex);
-            bytes memory payload = abi.encodeWithSignature("getDestinationReturnAmount(uint256,address,address,uint256,uint256)", routes[i], src, dest, srcAmount, partnerIndex);
-            (bool success, bytes memory data) = address(warden).staticcall(payload);
-            if (success) {
-                uint256 _amountOut = abi.decode(data, (uint256));
-                if (_amountOut > amountOut) {
-                    amountOut = _amountOut;
-                    routeIndex = routes[i];
-                }
+            uint256 route = routes[i];
+            uint256 _amountOut = _getRate(src, dest, srcAmount, route);
+            if (_amountOut > amountOut) {
+                amountOut = _amountOut;
+                routeIndex = routes[i];
             }
         }
     }
